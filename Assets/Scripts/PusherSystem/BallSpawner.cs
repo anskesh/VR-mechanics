12cc823592e74161cfa0353d97106da5d1d0bbf6
anskesh <laim207@mail.ru>
2024-03-04 11:14:59 +0300

    Add namespace to ball spawner

--- Assets/Scripts/PusherSystem/BallSpawner.cs
+++ Assets/Scripts/PusherSystem/BallSpawner.cs
@@ -1,71 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using PusherSystem;
 using PusherSystem.Configurations;
 using UnityEngine;
 
-public class BallSpawner : MonoBehaviour
+namespace PusherSystem
 {
-    [SerializeField] private Transform _spawnPoint;
-    [SerializeField] private SpawnerConfiguration _spawnerConfiguration;
-    
-    private List<Ball> _balls = new List<Ball>();
-    private bool _isSpawning = false;
-    
-    private void Awake()
+    public class BallSpawner : MonoBehaviour
     {
-        for (var i = 0; i < _spawnerConfiguration.CountOnStart; i++)
-            _balls.Add(Create());
+        [SerializeField] private Transform _spawnPoint;
+        [SerializeField] private SpawnerConfiguration _spawnerConfiguration;
+
+        private List<Ball> _balls = new List<Ball>();
+        private bool _isSpawning = false;
+
+        private void Awake()
+        {
+            for (var i = 0; i < _spawnerConfiguration.CountOnStart; i++)
+                _balls.Add(Create());
+        }
+
+        private void Start()
+        {
+            GetOrCreate();
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.TryGetComponent(out Ball ball) && _isSpawning == false)
+                StartCoroutine(Spawn());
+        }
+
+        private IEnumerator Spawn()
+        {
+            _isSpawning = true;
+            yield return new WaitForSeconds(_spawnerConfiguration.RespawnDelay);
+            GetOrCreate();
+            _isSpawning = false;
+        }
+
+        private void GetOrCreate()
+        {
+            if (!Get(out var ball))
+                ball = Create();
+
+            ball.transform.localPosition = Vector3.zero;
+            ball.transform.rotation = Quaternion.identity;
+            ball.gameObject.SetActive(true);
+        }
+
+        private bool Get(out Ball ball)
+        {
+            ball = null;
+
+            if (_balls.All(x => x.gameObject.activeSelf))
+                return false;
+
+            ball = _balls.First(x => x.gameObject.activeSelf == false);
+            return true;
+        }
+
+        private Ball Create()
+        {
+            var pushable = Instantiate(_spawnerConfiguration.Template, _spawnPoint);
+            pushable.gameObject.SetActive(false);
+            pushable.Init(_spawnerConfiguration.LifeTime);
+            _balls.Add(pushable);
+
+            return pushable;
+        }
     }
 
-    private void Start()
-    {
-        GetOrCreate();
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.TryGetComponent(out Ball ball) && _isSpawning == false)
-            StartCoroutine(Spawn());
-    }
-
-    private IEnumerator Spawn()
-    {
-        _isSpawning = true;
-        yield return new WaitForSeconds(_spawnerConfiguration.RespawnDelay);
-        GetOrCreate();
-        _isSpawning = false;
-    }
-
-    private void GetOrCreate()
-    {
-        if (!Get(out var ball))
-            ball = Create();
-        
-        ball.transform.localPosition = Vector3.zero;
-        ball.transform.rotation = Quaternion.identity;
-        ball.gameObject.SetActive(true);
-    }
-
-    private bool Get(out Ball ball)
-    {
-        ball = null;
-        
-        if (_balls.All(x => x.gameObject.activeSelf))
-            return false;
-            
-        ball = _balls.First(x => x.gameObject.activeSelf == false);
-        return true;
-    }
-
-    private Ball Create()
-    {
-        var pushable = Instantiate(_spawnerConfiguration.Template, _spawnPoint);
-        pushable.gameObject.SetActive(false);
-        pushable.Init(_spawnerConfiguration.LifeTime);
-        _balls.Add(pushable);
-
-        return pushable;
-    }
 }
